2d0054ecf6e5f7fa4b6641de7115558ef2695e6b
Candice <candice.vernacchio@gmail.com>
2021-11-03 17:38:55 -0600

    commit tree 663cd598c9c7f0a890e4c3664a1b126de8dc9e8c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+    const answer = drivers.filter((element) => element.toLowerCase() === string.toLowerCase())
+    return answer
+}
+//===============================
+function fuzzyMatch(drivers, letter){
+    return drivers.filter(element => {
+        return element.slice(0, letter.length).toLowerCase() === letter.toLowerCase() ? element : null
+    })
+}
+//================================
+function matchName(drivers, input){
+    let inputInfo = drivers.filter(object => object.name === input)
+        return inputInfo
+}
